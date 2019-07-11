a3d830daa7a5ed1baef465ab2ed0acb157ed8f50
Georgi Kolev <suncho_26@aol.com>
2019-07-11 10:47:57 +0200

    Add manually console

--- wp-content/plugins/wp-minitron-export/assets/admin/js/wpmt_admin.js
+++ wp-content/plugins/wp-minitron-export/assets/admin/js/wpmt_admin.js
@@ -9,11 +9,39 @@
     $(document).on('click', '#start-manually', function(event)
     {
         var data = {
-			'action': 'start_manually'
+			'action': 'batch_update'
 		};
         
         $.post(ajaxurl, data, function(response) {
-			console.log(response);
+            var response = JSON.parse(response);
+			if (response)
+            {
+                $.each(response, function (index, value) {
+                    setTimeout(function() {
+                        
+                        console.log(value);
+                        var dt = new Date();
+                        var currentHours = dt.getHours();
+                        currentHours = ("0" + currentHours).slice(-2);
+                        
+                        var currentSeconds = dt.getSeconds();
+                        currentSeconds = ("0" + currentSeconds).slice(-2);
+                        
+                        var time = currentHours + ":" + dt.getMinutes() + ":" + currentSeconds;
+                    
+                        // Draw div with data from the response
+                        var msgClass = (value.ok) ? 'green' : 'red';
+                        var html = '<div>';
+                            html += '<span class="time">' + time + '</span>';
+                            html += '<strong><em>#' + (index + 1) + '</em> '+ value.email +' </strong>';
+                            html += '<span class="msg ' + msgClass + '">' + value.msg + '</span>';
+                        html += '</div>';
+                        
+                        $('#wizard-console').append(html);
+                        
+                    }, 800 * index);
+                });
+            }
 		});
     });
 
